9140bf7c566e85c128e766137f27043ab220ab0b
davidtays <davidtays22@gmail.com>
2020-02-07 16:14:55 -0600

    Added BrowserRouter

--- src/components/main.js
+++ src/components/main.js
@@ -6,15 +6,20 @@ import Contact from './contact';
 import Projects from './projects';
 import Resume from './resume';
 
-import {Switch, Route} from 'react-router-dom';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 const Main = () => (
+    <BrowserRouter
+        basename="process.env.PUBLIC_URL + "
+    >
     <Switch>
-        <Route exact path={process.env.PUBLIC_URL + '/'} component={Landing} />
+        <Route exact path={'/'} component={Landing} />
         <Route exact path={process.env.PUBLIC_URL + '/components/aboutme'} component={About} />
         <Route exact path={process.env.PUBLIC_URL + '/components/contact'} component={Contact} />
         <Route exact path={process.env.PUBLIC_URL + '/components/projects'} component={Projects} />
         <Route exact path={process.env.PUBLIC_URL + '/components/resume'} component={Resume} />
     </Switch>
+    </BrowserRouter>
+    
 )
 export default Main;
