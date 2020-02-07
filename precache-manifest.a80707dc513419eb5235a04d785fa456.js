d5d8470637db9770ec36ccdf14223a5738255583
davidtays <davidtays22@gmail.com>
2020-02-06 21:12:41 -0600

    Updates

---
+++ precache-manifest.a80707dc513419eb5235a04d785fa456.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b96a7a3d9f92e3715783e544aec436c",
+    "url": "/my-react-portfolio/index.html"
+  },
+  {
+    "revision": "2fa2072c5ddbac0c178c",
+    "url": "/my-react-portfolio/static/css/2.559fd73f.chunk.css"
+  },
+  {
+    "revision": "8c65b2ff968a07701dfd",
+    "url": "/my-react-portfolio/static/css/main.dfdba76d.chunk.css"
+  },
+  {
+    "revision": "2fa2072c5ddbac0c178c",
+    "url": "/my-react-portfolio/static/js/2.720b93de.chunk.js"
+  },
+  {
+    "revision": "c7abd4a823ee85afac2f05677368b7de",
+    "url": "/my-react-portfolio/static/js/2.720b93de.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8c65b2ff968a07701dfd",
+    "url": "/my-react-portfolio/static/js/main.d3e949ac.chunk.js"
+  },
+  {
+    "revision": "6229cd3391dee0f4f7c9",
+    "url": "/my-react-portfolio/static/js/runtime-main.fe261ec1.js"
+  },
+  {
+    "revision": "b6fc80254d64e1140987c60f0d0af305",
+    "url": "/my-react-portfolio/static/media/david.b6fc8025.png"
+  }
+]);
