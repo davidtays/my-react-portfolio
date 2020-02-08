91ee112373eb1eae17d78c09b119d8c1227813d6
davidtays <davidtays22@gmail.com>
2020-02-07 19:00:47 -0600

    unregistered serviceworker and changed basename again

--- src/index.js
+++ src/index.js
@@ -9,7 +9,9 @@ import 'react-mdl/extra/material.js';
 
 
 ReactDOM.render(
-    <BrowserRouter>
+    <BrowserRouter
+        basename="/my-react-portfolio/"
+    >
         <App />  
     </BrowserRouter>      
     , document.getElementById('root'));
@@ -17,4 +19,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.unregister();
